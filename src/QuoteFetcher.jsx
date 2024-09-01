f0320d2078e2eb1e9ed92b25aaaf98a76b2cfd88
EmmaTheVibe <peteremmanuel316@gmail.com>
2024-09-01 12:46:02 +0100

    First commit

---
+++ src/QuoteFetcher.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
+
+export default function QuoteFetcher() {
+  const [quote, setQuote] = useState({ text: "", author: "" });
+
+  // useEffect(() => {
+  //   async function getInitialQuote() {
+  //     const response = await fetch(RANDOM_QUOTE_URL);
+  //     const jsonResponse = await response.json();
+  //     const randomQuote = jsonResponse.quote;
+  //     setQuote(randomQuote);
+  //   }
+  //   getInitialQuote();
+  // }, []);
+
+  // useEffect(() => {
+  //   fetchQuote();
+  // }, []);
+
+  async function fetchQuote() {
+    const response = await fetch(RANDOM_QUOTE_URL);
+    const jsonResponse = await response.json();
+    const randomQuote = jsonResponse.quote;
+    setQuote(randomQuote);
+  }
+
+  return (
+    <div>
+      <button onClick={fetchQuote}>Get Quote</button>
+      <h1 className="qrt">{quote.text}</h1>
+      <h3>
+        <em> -- {quote.author}</em>
+      </h3>
+    </div>
+  );
+}
